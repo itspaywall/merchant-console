83dbce291a55d16fa30127311e65c9e6b0f78e76
Samuel Rowe <samuelrowe1999@gmail.com>
2020-07-19 12:06:42 +0530

    Added a fake subscription generator in the mock backend.
    Two utility functions, `soonDate()` and `pastDate()`, were added to create random ISO date strings.
    The documentation of Faker.js specifies that `faker.date.soon()` is exported. However, on inspecting
    the module, it appears that `soon()` is not available. I think we are using an older version of
    Faker.js. We may want to update this in the future. Right now, `soonDate()` returns a future date.

--- src/mock-backend/backend.js
+++ src/mock-backend/backend.js
@@ -1,13 +1,24 @@
-import mock from "./mock";
 import faker from "faker";
+import { format } from "date-fns";
+import mock from "./mock";
 
 const DEFAULT_PLANS = 10;
 const DEFAULT_ACCOUNTS = 100;
+const DEFAULT_SUBSCRIPTIONS = 100 * 5;
 
 const plans = [];
 const accounts = [];
+const subscriptions = [];
 
 const periodUnits = ["days", "months"];
+
+function pastDate() {
+    return format(faker.date.past(), "yyyy/MM/dd");
+}
+
+function soonDate() {
+    return format(faker.date.future(), "yyyy/MM/dd");
+}
 
 // TODO: We could improve periods generated to be more realistic for months.
 function createPlan() {
@@ -28,12 +39,14 @@ function createPlan() {
             max: 6 * 30,
         }),
         trialPeriodUnit: faker.random.arrayElement(periodUnits),
+        starts: soonDate(),
         term: faker.random.number({
             min: 0,
             max: 6 * 30,
         }),
         termUnit: faker.random.arrayElement(periodUnits),
         renew: faker.random.boolean(),
+        createdOn: pastDate(),
     };
     return plan;
 }
@@ -54,8 +67,45 @@ function createAccount() {
         state: faker.address.state(),
         country: faker.address.country(),
         zipCode: faker.address.zipCode(),
+        subscriptions: [],
+        createdOn: pastDate(),
     };
     return account;
+}
+
+/* Subscriptions can have different values from the plans they are associated with.
+ * In other words, a plan is a template from which we create a subscription. However,
+ * when we generate a fake subscription here, we do not change the values. In the
+ * future, we may want to introduce this behavior.
+ *
+ * Technically, Hubble does not allow an account to hold multiple subscriptions to
+ * the same plan. We do not enforce such complex behavior here.
+ */
+function createSubscription() {
+    const plan = faker.random.arrayElement(plans);
+    const account = faker.random.arrayElement(accounts);
+    const accountCopy = { ...account };
+    delete accountCopy.subscriptions;
+
+    const subscription = {
+        identifier: faker.random.uuid(),
+        plan: plan,
+        account: accountCopy,
+        billingPeriod: plan.billingPeriod,
+        billingPeriodUnit: plan.billingPeriodUnit,
+        setupFee: plan.setupFee,
+        trialPeriod: plan.trialPeriod,
+        trialPeriodUnit: plan.trialPeriodUnit,
+        starts: plan.starts,
+        term: plan.term,
+        termUnit: plan.termUnit,
+        renew: plan.renew,
+        createdOn: pastDate(),
+    };
+
+    account.subscriptions.push(subscription);
+
+    return subscription;
 }
 
 function generateFakeData() {
@@ -63,13 +113,25 @@ function generateFakeData() {
         const plan = createPlan();
         plans.push(plan);
     }
-    console.log(plans);
 
     for (let i = 0; i < DEFAULT_ACCOUNTS; i++) {
         const account = createAccount();
         accounts.push(account);
     }
+
+    for (let i = 0; i < DEFAULT_SUBSCRIPTIONS; i++) {
+        const subscription = createSubscription();
+        subscriptions.push(subscription);
+    }
+
+    console.log("[plans]");
+    console.log(plans);
+
+    console.log("[accounts]");
     console.log(accounts);
+
+    console.log("[subscriptions]");
+    console.log(subscriptions);
 }
 
 generateFakeData();
